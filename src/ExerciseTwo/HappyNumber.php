ac924fe22b6492c7bb3d8fe992347acbea5b13cb
Silvanei Santos <silvanei.santos@objective.com.br>
2022-05-18 11:21:50 -0300

    Implementation exercise two
     No ramo main
     Mudanças a serem submetidas:
    	new file:   src/ExerciseTwo/HappyNumber.php
    	new file:   tests/ExerciseTwo/HappyNumberTest.php

---
+++ src/ExerciseTwo/HappyNumber.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SilvaneiSantos\BasicAutomatedTestTreining\ExerciseTwo;
+
+class HappyNumber
+{
+    public function isHappy(int $number): bool
+    {
+        if ($number < 1) {
+            return false;
+        }
+        $evaluatedNumbers = [];
+        while ($number > 1 && $this->wasNotEvaluated($number, $evaluatedNumbers)) {
+            $evaluatedNumbers[] = $number;
+            $number = $this->sumOfSquareOfDigits($number);
+        }
+        return $number === 1;
+    }
+
+    /**
+     * @param int $number
+     * @param array<int> $evaluatedNumbers
+     * @return bool
+     */
+    private function wasNotEvaluated(int $number, array $evaluatedNumbers): bool
+    {
+        return ! in_array($number, $evaluatedNumbers);
+    }
+
+    private function sumOfSquareOfDigits(int $number): int
+    {
+        $sumOfSquares = 0;
+        foreach ($this->digitsInANumber($number) as $digit) {
+            $sumOfSquares += pow($digit, 2);
+        }
+        return $sumOfSquares;
+    }
+
+    /**
+     * @param int $number
+     * @return array<int>
+     */
+    private function digitsInANumber(int $number): array
+    {
+        $digits = str_split("$number");
+        return array_map(fn($digit) => (int)$digit, $digits);
+    }
+}
---
+++ tests/ExerciseTwo/HappyNumberTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\SilvaneiSantos\BasicAutomatedTestTreining\ExerciseTwo;
+
+use PHPUnit\Framework\TestCase;
+use SilvaneiSantos\BasicAutomatedTestTreining\ExerciseTwo\HappyNumber;
+
+class HappyNumberTest extends TestCase
+{
+    public function testShouldReturnTrueForAHappyNumber(): void
+    {
+        $happyNumber = new HappyNumber();
+        $isAHappyNumber = $happyNumber->isHappy(7);
+        $this->assertTrue($isAHappyNumber);
+    }
+
+    public function testShouldReturnFalseForANonHappyNumber(): void
+    {
+        $happyNumber = new HappyNumber();
+        $isAHappyNumber = $happyNumber->isHappy(6);
+        $this->assertFalse($isAHappyNumber);
+        $isAHappyNumber = $happyNumber->isHappy(8);
+        $this->assertFalse($isAHappyNumber);
+    }
+
+    public function testShouldReturnFalseForNumberLessThanOne(): void
+    {
+        $happyNumber = new HappyNumber();
+        $isAHappyNumber = $happyNumber->isHappy(0);
+        $this->assertFalse($isAHappyNumber);
+    }
+}
